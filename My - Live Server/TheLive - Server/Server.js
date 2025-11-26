b5648b51bafaefbd68f5f34e5c15b7c1143326fa
DinzDev <dinzdev.webdeveloper.contact@gmail.com>
2025-11-25 22:30:50 -0300

    Add files via upload

---
+++ My - Live Server/TheLive - Server/Server.js
@@ -0,0 +1,29 @@
+
+
+const express = require("express");
+const WebSocket = require("ws");
+const chokidar = require("chokidar");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+const server = app.listen(PORT, () => {
+	console.log(`
+     Live server ligado em: http://localhost:${PORT}
+		`);
+});
+
+const wss = new WebSocket.server({ server });
+
+wss.on("connection", (ws) => {
+	console.log("Navegador conectado ao Live Reload!");
+});
+
+chokidar.watch("public").on("change", (filePath) => {
+  console.log(`Alterado: ${filePath}`);
+  wss.clients.forEach((client) => client.send("reload"));
+});
+
